f4619bb829a5bcaf716810866e823e053c51dec2
XenonCoder <xenon77.dev@gmail.com>
2018-03-20 19:57:49 +0100

    Added light

---
+++ src/com/xenonlab/skybox2d/Light.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2018 Stefano Peris
+ * eMail: <xenon77.dev@gmail.com>
+ * Github: https://github.com/XenonCoder
+ */
+
+package com.xenonlab.skybox2d;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public class Light {
+	
+	public static final BufferedImage LIGHT = ImageLoader.loadImage("img/light.png");
+	
+	private int x;
+	private int y;
+	private int radius;
+	
+	public Light(int x, int y, int radius) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+	}
+	
+	public void render(Graphics2D g) {
+		g.drawImage(LIGHT, x-radius, y-radius, radius*2, radius*2, null);
+	}
+}
